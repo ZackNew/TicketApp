831618b9ae506349a8c51da970fe7f1196f8d60e
zackNew <zekariyasmulugeta91@gmail.com>
2025-02-08 11:26:15 +0300

    feat: integrate Firestore for payments and todos; update dashboard layout with welcome message

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
+import { useCollection } from 'vuefire'
+import { collection, doc } from 'firebase/firestore'
+
 definePageMeta({
   middleware: ['auth'],
 })
+
+const db = useFirestore()
+const todos = useCollection(collection(db, 'payments'))
+const someTodo = useDocument(doc(collection(db, 'todos'), '00jUhL99VkrBuKjuuBdR'))
 </script>
 
 <template>
   <div class="max-w-7xl mx-auto rounded-xl border border-gray-200 shadow md:mt-20 mt-4 p-8">
-
+    <h1 class="text-2xl font-bold mb-4">Dashboard</h1>
+    <p>Welcome to the dashboard</p>
+    {{ someTodo }} pink
   </div>
 </template>
